969e87dafe941b602366c624fdc64cc889f48129
oreobird <winsonbird@gmail.com>
2017-06-20 08:01:01 -0700

    Add select in socket server.

--- socket/server.c
+++ socket/server.c
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
+#include <unistd.h>
 #include <sys/socket.h>
 #include <sys/select.h>
 #include <arpa/inet.h>
@@ -14,6 +15,9 @@
 #endif
 
 #define MAX_CLIENT 10
+#define BUFF_SIZE 1024
+
+#define MAX(a, b) ((a) > (b) ? (a) : (b))
 
 struct s_host {
     int fd;
@@ -24,7 +28,7 @@ struct s_host {
 struct s_client {
     int num;
     struct s_host cli[MAX_CLIENT];
-}
+};
 
 int sock_fd = -1;
 struct s_client g_client;
@@ -67,7 +71,7 @@ static int build_server(const char *ip, int port)
         return -1;
     }
 
-    ret = listen(sock_fd, 5);
+    ret = listen(sock_fd, MAX_CLIENT);
     if (ret < 0)
     {
         dbg("listen to addr %s failed\n", ip);
@@ -77,13 +81,13 @@ static int build_server(const char *ip, int port)
     return 0;
 }
 
-static int wait_for_connection(void)
+static int wait_for_client(void)
 {
     struct sockaddr_in addr;
     unsigned int addr_len = sizeof(addr);
 
-    g_client[g_client.num] = accept(sock_fd, (struct sockaddr*)&addr, &addr_len);
-    if (g_client[g_client.num] < 0)
+    g_client.cli[g_client.num].fd = accept(sock_fd, (struct sockaddr*)&addr, &addr_len);
+    if (g_client.cli[g_client.num].fd < 0)
     {
         dbg("accept client failed\n");
         return -1;
@@ -94,9 +98,109 @@ static int wait_for_connection(void)
     return 0;
 }
 
-static void handle_connection(void)
+static void init_client(void)
 {
+    int i = 0;
+    for (i = 0; i < MAX_CLIENT; i++)
+    {
+        g_client.cli[i].fd = -1;
+    }
+    g_client.num = 0;
+}
 
+static int handle_connect(fd_set rfds)
+{
+    struct sockaddr_in addr;
+    unsigned int addr_len;
+    int cli_fd = -1;
+    int i = 0;
+    
+    if (FD_ISSET(sock_fd, &rfds))
+    {
+        cli_fd = accept(sock_fd, (struct sockaddr*)&addr, &addr_len);
+        if (cli_fd == -1)
+        {
+            dbg("accept failed\n");
+            return -1;
+        }
+
+        for (i = 0; i < MAX_CLIENT; i++)
+        {
+            if (g_client.cli[i].fd < 0)
+            {
+                g_client.cli[i].fd = cli_fd;
+                g_client.cli[i].ip = addr.sin_addr.s_addr;
+                g_client.num++;
+                break;
+            }
+        }
+    }
+}
+
+static int handle_data(fd_set rfds, char *buf, int len)
+{
+    int i = 0;
+    int data_len = -1;
+
+    for (i = 0; i < MAX_CLIENT; i++)
+    {
+        if (g_client.cli[i].fd > 0 && FD_ISSET(g_client.cli[i].fd, &rfds))
+        {
+            data_len = recv(g_client.cli[i].fd, (char *) buf, len, 0);
+            if (data_len <= 0)
+            {
+                close(g_client.cli[i].fd);
+                g_client.cli[i].fd = -1;
+                g_client.num--;
+                break; 
+            }
+        }
+    }
+    return 0;
+}
+static void select_loop(void)
+{
+    fd_set rfds;
+    fd_set wfds;
+    char buf[BUFF_SIZE] = {0};
+    struct timeval timeout;
+    int i = 0;
+    int sock_max;
+    int ret;
+    
+    while (1)
+    {
+        FD_ZERO(&rfds);
+        FD_SET(sock_fd, &rfds);
+        sock_max = sock_fd;
+
+        for (i = 0; i < MAX_CLIENT; i++)
+        {
+            if (g_client.cli[i].fd > 0)
+            {
+                FD_SET(g_client.cli[i].fd, &rfds);
+                sock_max = MAX(sock_max, g_client.cli[i].fd);
+            }
+        }
+
+        timeout.tv_sec = 3;
+        timeout.tv_usec = 0;
+
+        ret = select((int) sock_max + 1, &rfds, NULL,NULL, &timeout);
+        if (ret < 0)
+        {
+            dbg("select error\n");
+            break;
+        } 
+        else if (ret == 0)
+        {
+            dbg("timeout ...\n");
+            continue;
+        }
+
+        handle_connect(rfds);
+        handle_data(rfds, buf, sizeof(buf));
+    }
 }
 
 int main(int argc, char **argv)
@@ -121,14 +225,8 @@ int main(int argc, char **argv)
         return -1;
     }
 
-    while (1)
-    {
-        if (wait_for_connection() < 0)
-        {
-            close(sock_fd);
-            return -1;
-        }
+    init_client();
+    select_loop();
 
-        handle_connection();
-    }
+    return 0;
 }
