6c158f63abb9a4d505e3a1458e6fab1ecbd92682
oreobird <winsonbird@gmail.com>
2017-09-26 19:10:01 -0700

    pthread:update pthread demo.

--- pthread/pthread_demo.c
+++ pthread/pthread_demo.c
@@ -1,15 +1,37 @@
 #include <stdio.h>
+#include <unistd.h>
 #include <pthread.h>
 
-struct s_net_status {
-    int status;
-    int (*set_net_status)(struct s_net_status *);
-    int (*get_net_status)(void);
-} g_net_status;
+void *fun1(void)
+{
+    while (1)
+    {
+        printf("this is fun 1\n");
+        sleep(2);
+    }
+}
 
+void *fun2(void)
+{
+    while (1)
+    {
+        printf("this is fun 2\n");
+        sleep(5);
+    }
+}
 
 int main(int argc, char **argv)
 {
+    pthread_t tid[2];
+    int i = 0;
+
+    pthread_create(&tid[0], NULL, (void*)fun1, (void *)NULL);
+    pthread_create(&tid[2], NULL, (void*)fun2, (void *)NULL);
+
+    for (i = 0; i < 2; i++)
+    {
+        pthread_join(tid[i], NULL);
+    }
 
     return 0;
 }
