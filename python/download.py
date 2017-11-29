c74b23d6e398c6e6dd336e382c8e41f0770e440a
oreobird <winsonbird@gmail.com>
2017-11-29 22:04:14 +0800

    add get imgcode get.

--- python/download.py
+++ python/download.py
@@ -8,7 +8,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 import time
-
+from PIL import Image
+import cv2
 
 class Download:
     def __init__(self, src_dir='', rar_urls=[]):
@@ -18,8 +19,27 @@ class Download:
         self.wanpan_pwd = []
         self.pwd_code = ['1024', '204601']
         self.rar_url = rar_urls
+        self.screenshot = 'E:\\test\\screenshot.png'
+        self.imgcode_src = 'F:\\python\\'
 
-    def rarfile_get(self):
+    def img_process(self, img_path = ''):
+        img = cv2.imread(img_path, 0)
+        cv2.imshow('imgcode', img)
+        blur = cv2.GaussianBlur(img, (5, 5), 0)
+        ret, bin_img = cv2.threshold(blur, 0, 255, cv2.THRESH_BINARY+cv2.THRESH_OTSU)
+        cv2.imshow('bin_img', bin_img)
+        cv2.waitKey(0)
+
+    def sample_get(self):
+        for root, dirs, files in os.walk(self.imgcode_src):
+            for f in files:
+                (filename, ext) = os.path.splitext(f)
+                if ext == '.jpg':
+                    img_path = os.path.join(root, f)
+                    print img_path
+                    self.img_process(img_path)
+
+    def imgcode_get(self):
         rar_url_num = len(self.rar_url)
         if rar_url_num > 0:
             driver = webdriver.Chrome()
@@ -39,6 +59,30 @@ class Download:
                 WebDriverWait(driver, 20, 1).until(EC.presence_of_element_located(locator))
                 item = driver.find_element_by_xpath('//*[@id="down_link"]/a[1]')
                 item.click()
+
+                for k in range(1, 100):
+                    locator = (By.XPATH, '//*[@id="imgcode"]')
+                    WebDriverWait(driver, 20, 1).until(EC.presence_of_element_located(locator))
+                    item = driver.find_element_by_xpath('//*[@id="imgcode"]')
+                    item.click()
+                    time.sleep(2)
+                    driver.get_screenshot_as_file(self.screenshot)
+                    left = int(item.location['x'])
+                    top = int(item.location['y'])
+                    right = left + int(item.size['width'])
+                    bottom = top + int(item.size['height'])
+                    orc = (left, top, right, bottom)
+                    print orc
+
+                    im = Image.open(self.screenshot)
+                    try:
+                        org_im = im.crop(orc)
+                    except SystemError:
+                        print "save error"
+                        time.sleep(5)
+                        continue
+                    org_im.save("%s%d.jpg" % (self.imgcode_src, k))
+                    time.sleep(3)
 
             time.sleep(20)
     def extract_files(self, pwd, file_path, dst):
@@ -162,7 +206,8 @@ class Download:
 
 if __name__ == '__main__':
     dl_obj = Download('F:\\test', ['http://www.777pan.cc/file-406077.html'])
-    dl_obj.rarfile_get()
+    #dl_obj.imgcode_get()
+    dl_obj.sample_get()
     #dl_obj.extract_all_rar()
     #dl_obj.parse_dl_info()
     #dl_obj.wanpan_dl()
